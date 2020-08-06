f760d6d72db04ae38c3ab0d16295f69dc71890f2
spencerwooo <spencerwoo98@foxmail.com>
2020-08-07 02:59:56 +0800

    fix: remove filesize render in external script

--- external/domUpdate.js
+++ external/domUpdate.js
@@ -25,31 +25,4 @@ async function renderReadme() {
     })
 }
 
-function humanFileSize(bytes, si) {
-  bytes = parseInt(bytes, 10)
-  var thresh = si ? 1000 : 1024
-  if (Math.abs(bytes) < thresh) {
-    return bytes + ' B'
-  }
-  var units = si
-    ? ['kB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB']
-    : ['KiB', 'MiB', 'GiB', 'TiB', 'PiB', 'EiB', 'ZiB', 'YiB']
-  var u = -1
-  do {
-    bytes /= thresh
-    ++u
-  } while (Math.abs(bytes) >= thresh && u < units.length - 1)
-  return bytes.toFixed(1) + ' ' + units[u]
-}
-
-document.addEventListener('DOMContentLoaded', function() {
-  document.querySelectorAll('a.item').forEach(function(i) {
-    i.insertAdjacentHTML(
-      'beforeend',
-      '<div style="flex-grow:1"></div>' +
-        (i.hasAttribute('size') ? '<span class="size">' + humanFileSize(i.getAttribute('size'), true) + '</span>' : '')
-    )
-  })
-})
-
 renderReadme().catch(console.error)

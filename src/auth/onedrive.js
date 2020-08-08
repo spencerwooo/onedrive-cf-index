14ad8803f40e395bc82ad6ae35c235bf2d1b7258
spencerwooo <spencerwoo98@foxmail.com>
2020-08-08 14:03:57 +0800

    feat: store tokens securely inside Firebase database

--- src/auth/onedrive.js
+++ src/auth/onedrive.js
@@ -1,15 +1,21 @@
 import config from '../config/default'
 
 /**
- * Current access token
- */
-let _accessToken = null
-
-/**
  * Get access token for microsoft graph API endpoints. Refresh token if needed.
  */
 export async function getAccessToken() {
-  if (_accessToken) return _accessToken
+  const timestamp = Math.floor(Date.now() / 1000)
+
+  // Fetch access token from Google Firebase Database
+  const data = await (
+    await fetch(`https://onedrive-cf-refresh-token.firebaseio.com/auth.json?auth=${FIREBASE_TOKEN}`)
+  ).json()
+  if (data && data.access_token && timestamp < data.expire_at) {
+    console.log('Fetched token from storage.')
+    return data.access_token
+  }
+
+  // Need refreshing, fetch new access token from Microsoft OneDrive API
   const resp = await fetch('https://login.microsoftonline.com/common/oauth2/v2.0/token', {
     method: 'POST',
     body: `client_id=${config.client_id}&redirect_uri=${config.redirect_uri}&client_secret=${config.client_secret}
@@ -19,10 +25,23 @@ export async function getAccessToken() {
     }
   })
   if (resp.ok) {
-    console.info('access_token refresh success.')
+    console.info('Successfully refreshed access_token.')
     const data = await resp.json()
-    _accessToken = data.access_token
-    return _accessToken
+
+    // Update expiration time at Google Firebase on token refresh
+    data.expire_at = timestamp + data.expires_in
+
+    const store = await fetch(`https://onedrive-cf-refresh-token.firebaseio.com/auth.json?auth=${FIREBASE_TOKEN}`, {
+      method: 'PUT',
+      body: JSON.stringify(data),
+      headers: {
+        'content-type': 'application/json'
+      }
+    })
+    console.log(store.status)
+
+    // Finally, return access token
+    return data.access_token
   } else {
     // eslint-disable-next-line no-throw-literal
     throw `getAccessToken error ${JSON.stringify(await resp.text())}`

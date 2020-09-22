789ef675a7ee114143911a843cd80f49ac4ef33f
Craig Hart <i@craigary.net>
2020-09-22 11:55:48 +0800

    :art: Use emoji instead of folder icon when possible

--- src/folderView.js
+++ src/folderView.js
@@ -38,11 +38,11 @@ export async function renderFolderView(items, path) {
 
   const el = (tag, attrs, content) => `<${tag} ${attrs.join(' ')}>${content}</${tag}>`
   const div = (className, content) => el('div', [`class=${className}`], content)
-  const item = (icon, fileName, fileAbsoluteUrl, size) =>
+  const item = (icon, fileName, fileAbsoluteUrl, size, emojiIcon) =>
     el(
       'a',
       [`href="${fileAbsoluteUrl}"`, 'class="item"', size ? `size="${size}"` : ''],
-      el('i', [`class="${icon}"`], '') +
+      (emojiIcon ? el('i', ['class="emoji"'], emojiIcon) : el('i', [`class="${icon}"`], '')) +
         fileName +
         el('div', ['style="flex-grow: 1;"'], '') +
         (fileName === '..' ? '' : el('span', ['class="size"'], readableFileSize(size)))
@@ -69,7 +69,16 @@ export async function renderFolderView(items, path) {
           (!isIndex ? item('far fa-folder', '..', `${path}..`) : '') +
             items
               .map(i => {
+                // Check if the current item is a folder or a file
                 if ('folder' in i) {
+                  // const emojiRegex = /(?:[\u2700-\u27bf]|(?:\ud83c[\udde6-\uddff]){2}|[\ud800-\udbff][\udc00-\udfff]|[\u0023-\u0039]\ufe0f?\u20e3|\u3299|\u3297|\u303d|\u3030|\u24c2|\ud83c[\udd70-\udd71]|\ud83c[\udd7e-\udd7f]|\ud83c\udd8e|\ud83c[\udd91-\udd9a]|\ud83c[\udde6-\uddff]|[\ud83c[\ude01-\ude02]|\ud83c\ude1a|\ud83c\ude2f|[\ud83c[\ude32-\ude3a]|[\ud83c[\ude50-\ude51]|\u203c|\u2049|[\u25aa-\u25ab]|\u25b6|\u25c0|[\u25fb-\u25fe]|\u00a9|\u00ae|\u2122|\u2139|\ud83c\udc04|[\u2600-\u26FF]|\u2b05|\u2b06|\u2b07|\u2b1b|\u2b1c|\u2b50|\u2b55|\u231a|\u231b|\u2328|\u23cf|[\u23e9-\u23f3]|[\u23f8-\u23fa]|\ud83c\udccf|\u2934|\u2935|[\u2190-\u21ff])/g
+                  const charRegex = /\w|\s/
+                  const firstNonEmojiPosition = i.name.search(charRegex)
+                  if (firstNonEmojiPosition !== 0) {
+                    const headerEmojiStr = i.name.slice(0, firstNonEmojiPosition)
+                    const folderName = i.name.slice(firstNonEmojiPosition).trim()
+                    return item('', folderName, `${path}${i.name}/`, i.size, headerEmojiStr)
+                  }
                   return item('far fa-folder', i.name, `${path}${i.name}/`, i.size)
                 } else if ('file' in i) {
                   // Check if README.md exists
